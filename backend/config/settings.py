dd781b8630c2d476106b327bf0de7b81f2d9b164
Janvi Ambedare <janviambedare@gmail.com>
2026-05-27 15:23:16 +0530

    Connect with railway db

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,11 +20,11 @@ class Settings:
     # =========================
     # DATABASE CONFIG
     # =========================
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = int(os.getenv("DB_PORT", 3306))
-    DB_NAME = os.getenv("DB_NAME", "guru_db")
-    DB_USER = os.getenv("DB_USER", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "janvi66")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = int(os.getenv("DB_PORT"))
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
 
     # =========================
     # STORAGE CONFIG
